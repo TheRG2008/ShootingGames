f19ff42b67e40163a9ed243831fbca9dad2717f6
TheRG <TheRG2008@gmail.com>
2021-04-03 02:20:15 +0500

    Добавил Player и скрипт передвижения

---
+++ ShootingGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1;
+    private Vector3 _dir;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        _dir.x = Input.GetAxis("Horizontal");
+        _dir.z = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        var speed = _speed * _dir * Time.deltaTime;
+        transform.Translate(speed);
+        _dir = Vector3.zero;
+    }
+}
