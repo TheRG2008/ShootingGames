40bb1619416cc8838dd0311953d58f71bc85a3d5
TheRG <TheRG2008@gmail.com>
2021-04-04 23:32:08 +0500

    Добавил ограничение по колличеству патронов

--- ShootingGame/Assets/Scripts/Shoot.cs
+++ ShootingGame/Assets/Scripts/Shoot.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
     [SerializeField] private GameObject _bullet; 
-    [SerializeField] private Transform _bulletSpawnPlace; 
+    [SerializeField] private Transform _bulletSpawnPlace;
+    [SerializeField] private int _countBullet = 8;
 
     private void Update()
     {
         
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            if (_countBullet > 0 )
             Instantiate(_bullet, _bulletSpawnPlace.position, _bulletSpawnPlace.rotation);
-            
+            _countBullet--;
         }
     }
 
